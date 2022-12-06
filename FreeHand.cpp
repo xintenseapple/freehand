f2fa38bb51ad7f3b30c46156385a2a49911f9c98
Evan Hellman <ehellman@iastate.edu>
2022-12-06 16:51:22 -0600

    Initial setup.

---
+++ FreeHand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evan on 12/6/2022.
+//
+
+#include "FreeHand.h"
---
+++ FreeHand.h
@@ -0,0 +1,15 @@
+//
+// Created by Evan on 12/6/2022.
+//
+
+#ifndef FREEHAND_FREEHAND_H
+#define FREEHAND_FREEHAND_H
+
+class FreeHand {
+
+    int main(int argc, char* argv[]);
+
+};
+
+
+#endif //FREEHAND_FREEHAND_H
